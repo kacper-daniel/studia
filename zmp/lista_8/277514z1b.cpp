0a62a279d16a5b6ee3974b202d6cb3eef8d742bc
Kacper Daniel <kacperd258@gmail.com>
2024-06-16 14:12:50 +0200

    poprawione zadanie 1b lista 8

--- zmp/lista_8/277514z1b.cpp
+++ zmp/lista_8/277514z1b.cpp
@@ -1,70 +1,117 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <set>
-#include <iterator>
-#include <string>
+#include <map>
 
-class Multiset {
-public:
-    std::multiset<int> elements;
+template <typename T> class Multiset{
+    private:
+        std::map<T, int> pairs;
 
-    void add(int element) {
-        elements.insert(element);
-    }
-
-    void display() const {
-        for (int element : elements) {
-            std::cout << element << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    bool isSubsetSumZero() const {
-        std::vector<int> vec(elements.begin(), elements.end());
-        std::unordered_map<std::string, bool> memo;
-        return isSubsetSumZeroRec(vec, vec.size(), 0, memo);
-    }
-
-private:
-    bool isSubsetSumZeroRec(const std::vector<int>& elements, int n, int sum, std::unordered_map<std::string, bool>& memo) const {
-        std::string key = std::to_string(n) + "_" + std::to_string(sum);
-        if (memo.find(key) != memo.end()) {
-            return memo[key];
+    public:
+        void add(T value, int count = 1){
+            pairs[value] += count;
         }
 
-        if (sum == 0) {
+        void remove(T value, int count = 1){
+            auto it = pairs.find(value);
+            if(it != pairs.end()){
+                it->second -= count;
+                if(it->second <= 0){
+                    pairs.erase(it);
+                }
+            }
+        }
+
+        bool operator==(const Multiset<T>& other) {
+            return pairs == other.pairs;
+        }
+
+        bool operator!=(const Multiset<T>& other){
+            return !(pairs == other.pairs);
+        }
+
+        bool isSubset(const Multiset<T>& other){
+            for(auto const& [key, val] : pairs){
+                auto it = other.pairs.find(key);
+                if (it == other.pairs.end() || it->second < pairs[key]){
+                    return false;
+                }
+            }
             return true;
         }
 
-        if (n == 0) {
-            return false;
+        Multiset<T> operator+(const Multiset<T>& other){
+            Multiset result = *this;
+            for (auto pair : other.pairs){
+                result.add(pair.first, pair.second);
+            }
+            return result;
         }
 
-        bool exclude = isSubsetSumZeroRec(elements, n - 1, sum, memo);
-        bool include = isSubsetSumZeroRec(elements, n - 1, sum - elements[n - 1], memo);
+        Multiset<T> operator*(const Multiset<T>& other){
+            Multiset<T> result;
+            for (auto pair : pairs){
+                auto it = other.pairs.find(pair.first);
+                if(it != other.pairs.end()){
+                    result.add(pair.first, std::min(pair.second, it->second));
+                }
+            }
+            return result;
+        }
 
-        memo[key] = include || exclude;
-        return memo[key];
-    }
+        Multiset<T> operator-(const Multiset<T>& other){
+            Multiset<T> result = *this;
+            for(auto pair : other.pairs){
+                result.remove(pair.first, pair.second);
+            }
+            return result;
+        }
+
+        void print(){
+            for(auto const& [key, val] : pairs){
+                std::cout << "(" << key << ", " << val << ") ";
+            }
+        }
 };
 
 int main() {
-    Multiset multiset;
+    Multiset<int> multiset1;
+    Multiset<int> multiset2;
 
-    int n;
+    int n, k;
     std::cin >> n;
+    std::cin >> k;
+
     for(int i = 0; i < n; i++){
         int temp;
-        std::cin >> temp;
-        multiset.add(temp);
+        std::cin>>temp;
+        multiset1.add(temp);
     }
 
-    if (multiset.isSubsetSumZero()) {
-        std::cout << "Istnieje podzbiór o sumie zero." << std::endl;
-    } else {
-        std::cout << "Nie istnieje podzbiór o sumie zero." << std::endl;
+    for(int i = 0; i < k; i++){
+        int temp;
+        std::cin>>temp;
+        multiset2.add(temp);
     }
+
+    std::cout << "M1: " << std::endl;
+    multiset1.print();
+
+    std::cout << "\nM2: " << std::endl;
+    multiset2.print();
+
+    Multiset<int> unionMultiset = multiset1 + multiset2;
+    Multiset<int> intersectionMultiset = multiset1 * multiset2;
+    Multiset<int> differenceMultiset = multiset1 - multiset2;
+
+    std::cout << "\nM1 + M2: " << std::endl;
+    unionMultiset.print();
+
+    std::cout << "\nM1 && M2:" << std::endl;
+    intersectionMultiset.print();
+
+    std::cout << "\nM1 \\ M2: " << std::endl;
+    differenceMultiset.print();
+
+    std::cout << "\nM1 < M2: " << multiset1.isSubset(multiset2) << std::endl;
 
     return 0;
 }
